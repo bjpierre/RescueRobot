27f17d2442ea6863d752f82b339c7a2fec502805
ryanjl9 <ryanjl9@iastate.edu>
2019-04-20 15:16:51 -0500

    Music Files

---
+++ music.h
@@ -0,0 +1,65 @@
+/*
+ * music.h
+ *
+ *  Created on: Apr 20, 2019
+ *      Author: ryanjl9
+ */
+
+#ifndef MUSIC_H_
+#define MUSIC_H_
+
+/*
+ * Tetris theme A
+ * 175 bpm
+ * 57 notes
+ * 2.91 bps
+ * Quarter Note = 22
+ *
+ * Megalovania
+ * 120 bpm
+ * 44 notes
+ * 2 bps
+ * Quarter Note = 32
+ *
+ * Middle C = 60
+ *
+ * QUARTER NOTE = (1/(BEATS_PER_MINUTE/60))*64
+ */
+
+#include <open_interface.h>
+
+#define LENGTH  65
+
+struct note{
+	char note;
+	char dur;
+};
+
+void set_song(int num);
+void load_song();
+void music_init(int numsong);
+int getSize(int num);
+struct note* getSong(int num);
+void setNote(int index, int note, int dur);
+
+int SLENGTH;
+int QUARTER;
+int EIGTH;
+int HALF;
+int WHOLE;
+int DOTTED;
+int SIXTEENTH;
+int DOTTED_EIGTH;
+
+#endif /* MUSIC_H_ */
+
+
+
+
+
+
+
+
+
+
+

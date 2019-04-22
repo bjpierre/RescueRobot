ccc409ca415f4ec9fec5d61e8fbc3ccaef90a2d7
adros <adros@iastate.edu>
2019-04-22 21:23:30 +0000

    Updating interrupt checks, bot position, and angle in movement.c

--- movement.c
+++ movement.c
@@ -28,29 +28,36 @@ int stop_movement = 0;
 void movement_init(oi_t* s){
 	sensor = s;
 
-	uint32_t temp[NUMINTERRUPTS] = {
-		s -> wheelDropLeft,
-		s -> wheelDropRight,
-		s -> bumpLeft,
-		s -> bumpRight,
-		s -> cliffLeft,
-		s -> cliffFrontLeft,
-		s -> cliffFrontRight,
-		s -> cliffRight
-	};
+// 	uint32_t temp[NUMINTERRUPTS] = {
+// 		s -> wheelDropLeft,
+// 		s -> wheelDropRight,
+// 		s -> bumpLeft,
+// 		s -> bumpRight,
+// 		s -> cliffLeft,
+// 		s -> cliffFrontLeft,
+// 		s -> cliffFrontRight,
+// 		s -> cliffRight
+// 	};
 
-	memcpy(sensor_interrupts, temp, NUMINTERRUPTS);
+// 	memcpy(sensor_interrupts, temp, NUMINTERRUPTS);
+
 }
 
 /**
  * move_forward: This method moves the CyBot forward
  */
-void move_forward() {
-	oi_setWheels(300, 300);							//Set wheel speed
+void move_forward(int centimeters) {
+	oi_setWheels(75, 75);							//Set wheel speed
+	int bot_distance = 0;
 
-	while (checkInterrupts() != 1 && stop_movement != 1) {					//As long as the total distance traveled is less than the
+
+	while (checkInterrupts() != 1 && stop_movement != 1 && bot_distance < (centimeters * 10.65 - 2.50)) {					//As long as the total distance traveled is less than the
 		oi_update(sensor);							//desired, keep going
+		bot_distance += sensor -> distance;
 	}
+    botX += (sum * 0.938 + 2.38 ) * cos(botAngle * 3.1416 / 180);
+    botY += (sum * 0.938 + 2.38 ) * sin(botAngle * 3.1416 / 180);
+
 
 	oi_setWheels(0, 0);
 }
@@ -58,23 +65,32 @@ void move_forward() {
 /**
  * move_backwards: This method moves the CyBot backwards
  */
-void move_backwards() {
-	oi_setWheels(-300, -300);
+void move_backwards(int centimeters) {
+	oi_setWheels(-75, -75);
+	int bot_distance = 0;
 
-	while (checkInterrupts() != 1 && stop_movement != 1) {
+	while (checkInterrupts() != 1 && stop_movement != 1 && bot_distance > (centimeters * -10.65 + 2.50)) {
 		oi_update(sensor);
+		bot_distance += sensor -> distance;
 	}
+	botX += (bot_distance * 0.938 + 2.38 ) * cos(botAngle * 3.1416 / 180);
+    botY += (bot_distance * 0.938 + 2.38 ) * sin(botAngle * 3.1416 / 180);
+    
+    oi_setWheels(0, 0);
 }
 
 /**
  * turn_right: This method turns the CyBot right
  */
-void turn_right() {
-	oi_setWheels(-200, 200);
+void turn_right(int degrees) {
+	oi_setWheels(-75, 75);
+	int bot_rotation = 0;
 
-	while (checkInterrupts() != 1 && stop_movement != 1) {
+	while (checkInterrupts() != 1 && stop_movement != 1 && bot_rotation > degrees * 0.93) {
 		oi_update(sensor);
+		bot_rotation += sensor -> angle;
 	}
+	botAngle += bot_rotation / 0.93;
 
 	oi_setWheels(0, 0);
 }
@@ -82,34 +98,42 @@ void turn_right() {
 /**
  * turn_left: This method turns the CyBot left
  */
-void turn_left() {
-	oi_setWheels(200, -200);						//Sets wheel speeds to opposites so the roomba spins left
+void turn_left(int degrees) {
+	oi_setWheels(75, -75);						//Sets wheel speeds to opposites so the roomba spins left
+    int bot_rotation = 0;
 
-	while (checkInterrupts() != 1 && stop_movement != 1) {
+	while (checkInterrupts() != 1 && stop_movement != 1 && bot_rotation < degrees * 0.93) {
 		oi_update(sensor);
+		bot_rotation += sensor -> angle;
 	}
+	botAngle += bot_rotation / 0.93;
 
 	oi_setWheels(0, 0);
 }
 
 void move(){
 	if(direction[0] == 1){
-		move_forward();
+		move_forward(10);
 	}else if(direction[1] == 1){
-		turn_left();
+		turn_left(30);
 	}else if(direction[2] == 1){
-		move_backwards();
+		move_backwards(10);
 	}else if(direction[3] == 1){
-		turn_right();
+		turn_right(30);
 	}
 }
 
 int checkInterrupts(){
-	int i;
+// 	int i;
 
-	for(i = 0; i < NUMINTERRUPTS; i++){
-		if(sensor_interrupts[i] == 1) return 1;
+// 	for(i = 0; i < NUMINTERRUPTS; i++){
+// 		if(sensor_interrupts[i] == 1) return 1;
+// 	}
+	
+	if ( (sensor -> bumpLeft) || (sensor -> bumpRight) || (sensor -> wheelDropLeft) || (sensor -> wheelDropRight) || (sensor -> bumpLeft) || (sensor -> bumpRight) || (sensor -> cliffLeft) || (sensor -> cliffFrontLeft) || (sensor -> cliffFrontRight) || (sensor -> cliffRight)) {
+	   return 1;
 	}
+
 	return 0;
 }
 
